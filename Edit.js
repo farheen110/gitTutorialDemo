b8d2df6d3af8236acafb60ab3fa99f35da0c0459
shaikhfarheen11 <farheens554>
2023-03-05 02:18:21 +0530

    first commit

---
+++ Edit.js
@@ -0,0 +1,42 @@
+
+function saveTolocalStorage(event){
+   event.preventDefault();
+   const name = event.target.name.value;
+   const email = event.target.email.value;
+   const phonenumber = event.target.phonenumber.value;
+
+   const obj = {
+      name,
+      email, 
+      phonenumber
+   }
+   localStorage.setItem(obj.email, JSON.stringify(obj));
+   showUserOnScreen(obj);
+}
+function showUserOnScreen(obj){
+   const parentElement = document.getElementById('listOfitems');
+   const childElement = document.createElement('li');
+   childElement.textContent = obj.name + ' - ' + obj.email + ' - ' + obj.phonenumber 
+
+   const deleteBtn = document.createElement('input');
+   deleteBtn.type = "button";
+   deleteBtn.value = 'Delete';
+   deleteBtn.onclick = () => {
+      localStorage.removeItem(obj.email);
+      localStorage.removeChild(childElement);
+   }
+   const editBtn = document.createElement('input');
+   editBtn.type = "button";
+   editBtn.value = 'Edit';
+
+   editBtn.onclick = () => {
+      localStorage.removeItem(obj.email);
+      localStorage.removeChild(childElement);
+      
+   }
+   childElement.appendChild(deleteBtn);
+   childElement.appendChild(editBtn);
+   parentElement.appendChild(childElement);
+   
+   
+} 
